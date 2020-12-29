e25b578ff597b202a0436bfe31fd1d2b0d1a6c1d
TomRiddle123 <47905293+TomRiddle123@users.noreply.github.com>
2020-12-29 17:17:18 +0200

    Added a file that will contain a game handling class with docstring

---
+++ online_game_handler.py
@@ -0,0 +1,13 @@
+"""
+    Name: online_game_handler.py
+
+    Description: This file contains the class of the online game handler - it sends the user guesses and receives the
+                 rival's guesses - handles the game from the board input until the end.
+
+    Usage: from online_game_handler import OnlineGameHandler
+
+    Author: Guy Sugbeker
+
+    Change Log:
+    29/12/2020 - Created
+"""
