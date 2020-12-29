4f3fdb4828319eb66a30af252eee30db064b4b6a
TomRiddle123 <47905293+TomRiddle123@users.noreply.github.com>
2020-12-29 11:18:30 +0200

    Added the file for the game manager and added a basic docstring to it

---
+++ Game/game_manager.py
@@ -0,0 +1,13 @@
+"""
+    Name: game_manager.py
+
+    Description: This file has a class of game manager - it contains the main control of the game - a board with
+                 submarines on it that returns an answer of an attack attempt.
+
+    Usage: from Game.game_manager import GameManager
+
+    Author: Guy Sugbeker
+
+    Change Log:
+    29/12/2020 - Crates
+"""
