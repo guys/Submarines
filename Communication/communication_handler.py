4271770601a5b971a2a059242239568d5d102401
TomRiddle123 <47905293+TomRiddle123@users.noreply.github.com>
2020-12-29 15:47:56 +0200

    Added the file that will hold the communication handler that will work according to the protocol.

---
+++ Communication/communication_handler.py
@@ -0,0 +1,12 @@
+"""
+    Name: communication_handler.py
+
+    Description: This file has the class of the communication handler that works according to protocol.
+
+    Usage: from Communication.communication_handler import CommunicationHandler
+
+    Author: Guy Sugbeker
+
+    Change Log:
+    29/12/2020 - Created
+"""
