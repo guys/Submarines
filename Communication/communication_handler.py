7e9277abd3a0c5ff762c1a45781540de5103e167
TomRiddle123 <47905293+TomRiddle123@users.noreply.github.com>
2020-12-29 16:13:00 +0200

    Added the class of the communication handler with a ctor and a function to wait for connection - still a lot to do

--- Communication/communication_handler.py
+++ Communication/communication_handler.py
@@ -10,3 +10,35 @@
     Change Log:
     29/12/2020 - Created
 """
+from socket import socket
+COMMUNICATION_PORT = 1234
+FIELD_SIZE = 1
+LOCAL_ADDRESS = "0.0.0.0"
+
+
+class CommunicationHandler:
+    """
+    a class that its instances help to control the communication according to the given protocol.
+    """
+    def __init__(self, comm_socket):
+        """
+        a C'tor for the handler - gets the socket on which the data will be transferred.
+        :param socket comm_socket: the socket that will be used to communicate.
+        :return: a new CommunicationHandler is created.
+        """
+        self.comm_socket = comm_socket
+        self.game_socket = None
+
+    def wait_for_player(self, wanted_ip):
+        """
+        this function is used when we are the host of the game. it waits for a specific ip to connect, initializes the
+        communication and returns a code that represent the starting player
+        :param str wanted_ip: the ip that we want to join us on a game
+        :return:
+        """
+        connected_address = ""
+        self.comm_socket.listen()
+        while connected_address != wanted_ip:
+            self.game_socket, (connected_address, _) = self.comm_socket.accept()
+
+
