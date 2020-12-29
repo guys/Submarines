9992d2ad267d2276b74d3028d1d647111bc06319
TomRiddle123 <47905293+TomRiddle123@users.noreply.github.com>
2020-12-29 17:22:36 +0200

    Added a skeleton for the OnlineGameHandler with a basic C'tor.

--- online_game_handler.py
+++ online_game_handler.py
@@ -11,3 +11,20 @@
     Change Log:
     29/12/2020 - Created
 """
+
+
+class OnlineGameHandler:
+    """
+    a class that its instances control the game in the client side - runs the game as rounds from the first round until
+    someone wins.
+    """
+
+    def __init__(self, i_start, game_socket):
+        """
+        a C'tor for the OnlineGameHandler.
+        :param bool i_start: True if we start the game, False otherwise.
+        :param socket.socket game_socket: the socket on which the game runs.
+        :return: a new OnlineGameHandler is created.
+        """
+        self.my_turn = i_start
+        self.game_socket = game_socket
