a92d72fb092f6a9afc5dbf2b56d122377e7f792e
TomRiddle123 <47905293+TomRiddle123@users.noreply.github.com>
2020-12-30 10:51:05 +0200

    Added the main program of the game with a docstring.

---
+++ submarines.py
@@ -0,0 +1,12 @@
+"""
+    Name: submarines.py
+
+    Description: This program initializes everything for the submarine game and starts it according to the user wishes.
+
+    Usage: python submarines.py [ip_address]
+
+    Author: Guy Sugbeker
+
+    Change Log:
+    30/12/2020 - Created
+"""
